63f2978fbc2c991dd6ad329f2aa5bdb97ace6bf2
Ranjeet poojari <ranjeetpoojari77@gmail.com>
2023-06-13 17:27:44 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css"
+        integrity="sha512-OTcub78R3msOCtY3Tc6FzeDJ8N9qvQn1Ph49ou13xgA9VsH9+LRxoFU6EqLhW4+PKRfU+/HReXmSZXHEkpYoOA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>HitKarni College</title>
+</head>
+
+<body>
+        <div class="container">
+        <div class="row college_details">
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="card">
+                    <div class="card-header d-flex justify-content-center align-items-center">
+                        Announcement
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Admission Process 2023-24 <span
+                                    class="badge bg-danger d-inline-block ms-5">New</span></li>
+                            <li class="list-group-item">Invitation for Justice B.D. Bal Memorial Lecture : 13th May 2023
+                                at 11.00 am</li>
+                            <li class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting
+                                industry. Lorem Ipsum has been the industry's standard</li>
+                            <li class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting
+                                industry</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="card">
+                    <div class="card-header d-flex justify-content-center align-items-center">
+                        Notice Board
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Admission Process 2023-24 <span
+                                    class="badge bg-danger d-inline-block ms-5">New</span></li>
+                            <li class="list-group-item">Bachelor of Commerce (B.Com.)</li>
+                            <li class="list-group-item">Bachelor of Management Studies (B.M.S.)</li>
+                            <li class="list-group-item">Bachelor of Commerce in Accounts & Finance (B.A.F.)</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="card m-aut">
+                    <div class="card-header d-flex justify-content-center align-items-center">
+                        Events
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Admission Process 2023-24 <span
+                                    class="badge bg-danger d-inline-block ms-5">New</span></li>
+                            <li class="list-group-item">Invitation for Justice B.D. Bal Memorial Lecture : 13th May 2023
+                                at 11.00 am</li>
+                            <li class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting
+                                industry. Lorem Ipsum has been the industry's standard</li>
+                            <li class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting
+                                industry</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid details_facalty my-5">
+        <div class="container">
+            <p class="text-light text-center">
+                hitkarni is placed at the forefront of excellent education because of its academic quality, research
+                focus and faculty from top national and global institutes & industries. Pursue a great career from a
+                recognized platform. Create a better tomorrow with us.
+            </p>
+            <div class="num_container row">
+                <div class="num_box col-12 col-sm-6 col-md-3">
+                    <div class="num text-center">
+                        25
+                    </div>
+                    <div class="num-info">
+                        Years of legacy
+                    </div>
+                </div>
+                <div class="num_box col-12 col-sm-6 col-md-3">
+                    <div class="num text-center">
+                        26.2k+
+                    </div>
+                    <div class="num-info">
+                        Full-time Students
+                    </div>
+                </div>
+                <div class="num_box col-12 col-sm-6 col-md-3">
+                    <div class="num text-center">
+                        120+
+                    </div>
+                    <div class="num-info">
+                        Faculty Memeber
+                    </div>
+                </div>
+                <div class="num_box col-12 col-sm-6 col-md-3">
+                    <div class="num text-center">
+                        3
+                    </div>
+                    <div class="num-info">
+                        Specialized schools
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container my-5 news_blogs_con">
+        <h1 class="text-center">New & Blogs</h1>
+        <div class="news-blogs mt-4 d-flex justify-content-center">
+            <div class="owl-carousel owl-theme new_carousel">
+                <div class="item">
+                    <div class="card">
+                        <img src="assets\demo1.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Lorem Ipsum is simply dummy text of the printing and typesetting</h5>
+                            <p class="card-text"><small class="date">26 June 2023</small></p>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <p class="text-end"><a href="">Readmore&nbsp;&nbsp; <i
+                                        class="fa-solid fa-arrow-right-long"></i></a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="card">
+                        <img src="assets\demo2.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Lorem Ipsum is simply dummy text of the printing and typesetting</h5>
+                            <p class="card-text"><small class="date">26 June 2023</small></p>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <p class="text-end"><a href="">Readmore&nbsp;&nbsp; <i
+                                        class="fa-solid fa-arrow-right-long"></i></a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="card">
+                        <img src="assets\demo3.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Lorem Ipsum is simply dummy text of the printing and typesetting</h5>
+                            <p class="card-text"><small class="date">26 June 2023</small></p>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <p class="text-end"><a href="">Readmore&nbsp;&nbsp; <i
+                                        class="fa-solid fa-arrow-right-long"></i></a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="card">
+                        <img src="assets\demo4.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Lorem Ipsum is simply dummy text of the printing and typesetting</h5>
+                            <p class="card-text"><small class="date">26 June 2023</small></p>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <p class="text-end"><a href="">Readmore&nbsp;&nbsp; <i
+                                        class="fa-solid fa-arrow-right-long"></i></a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="card">
+                        <img src="assets\demo4.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Lorem Ipsum is simply dummy text of the printing and typesetting</h5>
+                            <p class="card-text"><small class="date">26 June 2023</small></p>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <p class="text-end"><a href="">Readmore&nbsp;&nbsp; <i
+                                        class="fa-solid fa-arrow-right-long"></i></a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+
+            </div>
+            <div class="col-6"></div>
+        </div>
+    </div>
+    <div class="container gallary_container">
+        <div class="gallary-items gallary-items1">
+            <img src="assets\gal1.png" alt="" srcset="">
+        </div>
+        <div class="gallary-items gallary-items2">
+            <img src="assets\gal6.jpg" alt="" srcset="">
+        </div>
+        <div class="gallary-items gallary-items3">
+            <img src="assets\gal2.png" alt="" srcset="">
+        </div>
+        <div class="gallary-items gallary-items4">
+            <img src="assets\gal3.png" alt="" srcset="">
+        </div>
+        <div class="gallary-items gallary-items5">
+            <img src="assets\gal4.png" alt="" srcset="">
+        </div>
+        <div class="gallary-items gallary-items6">
+            <img src="assets\gal5.png" alt="">
+        </div>
+        <div class="gallary-items gallary-items7">
+            <img src="assets\gal8 (1).png" alt="">
+        </div>
+        <div class="gallary-items gallary-items8">
+            <img src="assets\gal8 (2).png" alt="">
+        </div>
+
+
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"
+        integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- owl-carousel -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
+        integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+        integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+        crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+</body>
+
+</html>
